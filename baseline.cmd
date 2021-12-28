fe728e1bf9eccd0e628bdde8c6891ddb3f700181
breakfast1234 <truongmeric@gmail.com>
2021-12-28 03:07:54 -0800

    Changed packages to ones I care about
    Will need to update the README

--- baseline.cmd
+++ baseline.cmd
@@ -29,37 +29,26 @@ ECHO UPDATES
 ECHO ============================
 :: Developer Updates
 choco upgrade python -y
-choco upgrade yarn -y
 choco upgrade git -y
-choco upgrade gh -y
-choco upgrade microsoft-edge -y
 choco upgrade googlechrome -y
-choco upgrade firefox -y
+choco upgrade opera-gx -y
 choco upgrade vscode -y
-choco upgrade azure-cli -y
-choco upgrade microsoftazurestorageexplorer -y
-choco upgrade azure-functions-core-tools -y
-choco upgrade azurepowershell -y
-choco upgrade azure-data-studio -y
-choco upgrade docker-desktop -y
-choco upgrade filezilla -y
 choco upgrade 7zip -y
-choco upgrade notepadplusplus -y
-choco upgrade audacity -y
-:: Meetings
-choco upgrade skype -y
-choco upgrade microsoft-teams -y
-choco upgrade webex-meetings -y
-choco upgrade zoom -y
+choco upgrade sublimetext4 -y
+:: Gaming
+choco upgrade goggalaxy -y
+choco upgrade epicgameslauncher -y
+choco upgrade discord -y
+choco upgrade steam-client -y
 :: Video
 choco upgrade obs-studio -y
 choco upgrade vlc -y
 :: Utilities
-choco upgrade rufus -y
 choco upgrade ccleaner -y
-choco upgrade cpu-z -y
-choco upgrade steam -y
-choco upgrade grammarly -y
+choco upgrade revo-uninstaller -y
 choco upgrade choco-cleaner -y
+choco upgrade bitwarden -y
+choco upgrade malwarebytes -y
+choco upgrade bitwarden -y
 :: Chocolatey
 choco upgrade chocolatey -y
